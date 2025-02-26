f6957253844119e0a279e9ae2fc577dd52603c8e
elca-hub <maomuhuangying090@gmail.com>
2025-02-27 01:20:48 +0900

    UPdate: 前処理を関数化

--- back/usecase/user/create_user_test.go
+++ back/usecase/user/create_user_test.go
@@ -1,55 +1,57 @@
 package user
 
 import (
-	"devport/domain/model"
+	usermodel "devport/domain/model"
 	mock_nosql "devport/domain/repository/mock/nosql"
 	mock_sql "devport/domain/repository/mock/sql"
 	mock_email "devport/infra/mock/email"
 	"github.com/stretchr/testify/assert"
 	"go.uber.org/mock/gomock"
 	"testing"
-	"time"
 )
+
+func beforeAction(t *testing.T, i CreateUserInput) (
+	CreateUserUseCase,
+	*mock_sql.MockUserRepository,
+	*mock_nosql.MockUserRepository,
+	*mock_email.MockEmail,
+) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	sqlMock := mock_sql.NewMockUserRepository(ctrl)
+	noSqlMock := mock_nosql.NewMockUserRepository(ctrl)
+	emailMock := mock_email.NewMockEmail(ctrl)
+
+	uc := NewCreateUserInterator(sqlMock, noSqlMock, emailMock)
+
+	return uc, sqlMock, noSqlMock, emailMock
+}
 
 func TestCreateUser(t *testing.T) {
 	t.Run("success", func(t *testing.T) {
-		testEmail, _ := model.NewEmail("test@example.com")
-		testModel, _ := model.NewUser(
-			model.NewUUID(""),
-			"test",
-			20,
-			testEmail,
-			"password0123",
-			time.Now(),
-			time.Now(),
-			model.Unconfirmed,
-		)
+		i := CreateUserInput{
+			Age:      18,
+			Name:     "test",
+			Email:    "test@example.com",
+			Password: "security",
+		}
 
-		ctrl := gomock.NewController(t)
+		testEmail, _ := usermodel.NewEmail(i.Email)
 
-		defer ctrl.Finish()
-
-		sqlMock := mock_sql.NewMockUserRepository(ctrl)
-		noSqlMock := mock_nosql.NewMockUserRepository(ctrl)
-		emailMock := mock_email.NewMockEmail(ctrl)
+		uc, sqlMock, noSqlMock, emailMock := beforeAction(t, i)
 
 		sqlMock.EXPECT().Create(gomock.Any()).Return(nil)
 		sqlMock.EXPECT().Exists(testEmail).Return(false, nil)
 		noSqlMock.EXPECT().StartSession(testEmail)
-		emailMock.EXPECT().SendEmail([]string{testEmail.Email()}, "【メール確認のお願い】", gomock.Any())
+		emailMock.EXPECT().SendEmail([]string{i.Email}, gomock.Any(), gomock.Any())
 
-		interator := NewCreateUserInterator(sqlMock, noSqlMock, emailMock)
-
-		input := CreateUserInput{
-			Age:      testModel.Age(),
-			Name:     testModel.Name(),
-			Email:    testEmail.Email(),
-			Password: testModel.Password(),
-		}
-
-		output, err := interator.Execute(input)
+		res, err := uc.Execute(i)
 
 		assert.NoError(t, err)
-		assert.Equal(t, output.Email, testEmail.Email())
+
+		assert.Equal(t, i.Email, res.Email)
+	})
+
 	})
 }
