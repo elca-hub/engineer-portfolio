c354aeeb360fd5d5397b6caec37d43281387ebe5
elca-hub <maomuhuangying090@gmail.com>
2025-03-02 01:32:09 +0900

    Update: パスワードのテストケースをuserテストケースから分割

--- back/domain/model/user_test.go
+++ back/domain/model/user_test.go
@@ -12,6 +12,12 @@ func fetchEmail() *Email {
 	return email
 }
 
+func fetchPassword() *HashedPassword {
+	hashed := NewHashedPassword("test")
+
+	return hashed
+}
+
 func TestUser(t *testing.T) {
 	t.Run("success", func(t *testing.T) {
 		_, err := NewUser(
@@ -19,7 +25,7 @@ func TestUser(t *testing.T) {
 			"test",
 			0,
 			fetchEmail(),
-			"Security_3939",
+			fetchPassword(),
 			time.Now(),
 			time.Now(),
 			Unconfirmed,
@@ -61,7 +67,7 @@ func TestUser(t *testing.T) {
 						c.name,
 						0,
 						fetchEmail(),
-						"Security_3939",
+						fetchPassword(),
 						time.Now(),
 						time.Now(),
 						Unconfirmed,
@@ -89,63 +95,7 @@ func TestUser(t *testing.T) {
 						"test",
 						c.age,
 						fetchEmail(),
-						"Security_3939",
-						time.Now(),
-						time.Now(),
-						Unconfirmed,
-					)
-
-					assert.Error(t, err)
-				})
-			}
-		})
-
-		t.Run("Password", func(t *testing.T) {
-			tooShortPassword := ""
-			for i := 0; i < MinPasswordLen-1; i++ {
-				tooShortPassword += "a"
-			}
-
-			tooLongPassword := ""
-			for i := 0; i < MaxPasswordLen+1; i++ {
-				tooLongPassword += "a"
-			}
-
-			cases := map[string]struct {
-				password string
-			}{
-				"empty": {
-					password: "",
-				},
-				"too short": {
-					password: tooShortPassword,
-				},
-				"too long": {
-					password: tooLongPassword,
-				},
-				"not using special characters": {
-					password: "security3939",
-				},
-				"not using numbers": {
-					password: "security_",
-				},
-				"not using lowercase letters": {
-					password: "SECURITY_3939",
-				},
-				"not using uppercase letters": {
-					password: "security_3939",
-				},
-			}
-
-			for name, c := range cases {
-				t.Run(name, func(t *testing.T) {
-					t.Parallel()
-					_, err := NewUser(
-						NewUUID(""),
-						"test",
-						0,
-						fetchEmail(),
-						c.password,
+						fetchPassword(),
 						time.Now(),
 						time.Now(),
 						Unconfirmed,
