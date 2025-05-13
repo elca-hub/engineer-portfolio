5ea581b27c6b6f0b299ee14793f062c1bded1bc6
elca-hub <maomuhuangying090@gmail.com>
2025-05-13 13:30:31 +0900

    Update: 自己紹介sectionのpadding修正

--- front/app/_containers/profile/presentation.tsx
+++ front/app/_containers/profile/presentation.tsx
@@ -129,7 +129,7 @@ export default function ProfilePresentation({ header, user, isAuthUser, external
 
 				<section>
 					{bio ? (
-						<div className="mt-4 bg-white rounded-md p-10">
+						<div className="mt-4 bg-white rounded-md py-6 px-10">
 							<h2 className="text-2xl font-bold mb-6">自己紹介</h2>
 							<CustomMarkdown>{bio}</CustomMarkdown>
 						</div>
